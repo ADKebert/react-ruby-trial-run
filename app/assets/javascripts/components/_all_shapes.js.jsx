ffe9b932426611f5723604e1ad7361f551f0e052
Brett Macy <bam03584@aol.com>
2016-09-03 23:09:25 -0400

    minor styling added

--- app/assets/javascripts/components/_all_shapes.js.jsx
+++ app/assets/javascripts/components/_all_shapes.js.jsx
@@ -1,7 +1,7 @@
 const AllShapes = React.createClass({
   render() {
     const shapes = this.props.shapes.map((shape) => {
-      return <div key={shape.id}>
+      return <div key={shape.id} className="shapes">
           <Shape shape={shape}
                 delete={this.props.delete}
                 edit={this.props.edit} />
--- app/assets/javascripts/components/_shape.js.jsx
+++ app/assets/javascripts/components/_shape.js.jsx
@@ -15,15 +15,15 @@ const Shape = React.createClass({
       const shape = {id: id, name: name, color: color}
       this.props.edit(shape)
     }
-    
+
     this.setState({editable: !this.state.editable})
 
   },
 
   render () {
     const { shape } = this.props
-    let name = this.state.editable ? <input type='text' ref='name' defaultValue={shape.name} /> : <h3>{shape.name}</h3>
-    let color = this.state.editable ? <input type='text' ref='color' defaultValue={shape.color} /> : <p>{shape.color}</p>;
+    let name = this.state.editable ? <input type='text' ref='name' defaultValue={shape.name} /> : <h3>Shape: {shape.name}</h3>
+    let color = this.state.editable ? <input type='text' ref='color' defaultValue={shape.color} /> : <p>Color: {shape.color}</p>;
     let date = moment(shape.created_at).format('YYYY MM DD')
     let update = moment(shape.updated_at).format("dddd, MMMM Do YYYY, h:mm:ss a")
     return <div>
